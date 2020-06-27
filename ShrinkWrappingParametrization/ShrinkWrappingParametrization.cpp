2d6247c5c5b3e6ddef0b44f5372a10a245a97950
Takashi IJIRI <takashi.ijiri80@gmail.com>
2020-06-27 17:49:28 +0900

    first commit
    first commit: a project was generated

---
+++ ShrinkWrappingParametrization/ShrinkWrappingParametrization.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+using namespace System;
+
+
+
+//todo 
+//1 first commit 
+//2 generate form 
+//3 vis opengl
+//4 load objs
+//5 get depthmap
+//6 get depth surface 
+//7 shrinkWrapping
+
+
+
+int main(array<System::String ^> ^args)
+{
+    return 0;
+}
